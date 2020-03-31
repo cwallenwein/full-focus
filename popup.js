a6d59e9ff87cd54313149cd1f7024c15812c415b
chriswallenwein <chris.wallenwein@gmail.com>
2020-03-31 10:32:53 +0200

    rearrange files in folders

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var checkPageButton = document.getElementById('checkPage');
-    checkPageButton.addEventListener('click', function(){
-        
-    }, false);
-}, false);
