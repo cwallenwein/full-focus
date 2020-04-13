2ed1752479ef7a70fe72c908886dd94502263c7e
chriswallenwein <chris.wallenwein@gmail.com>
2020-04-13 17:30:38 +0200

    dynamically check boxes based on settings

--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,10 @@
 // request all settings from storage when popup is opened
-chrome.storage.sync.get('settings_youtube', function(response){
-    document.getElementById("homepage").checked = response.settings_youtube.hideHomepage;
-    document.getElementById("comments").checked = response.settings_youtube.hideComments;
-    document.getElementById("playlists").checked = response.settings_youtube.hidePlaylists;
-    document.getElementById("recommendations").checked = response.settings_youtube.hideRecommendations;
+chrome.storage.sync.get('settings', function(response){
+    console.log(response)
+    var settings = response.settings.youtube
+    for(var key in settings){
+        document.getElementById(key).checked = settings[key]
+    }
 })
 
 
