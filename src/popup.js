7ab5b5b78882e124df6a18371c5bc7f33ca2159d
chriswallenwein <chris.wallenwein@gmail.com>
2020-04-13 21:37:52 +0200

    add button to settings to turn extension on/off

--- src/popup.js
+++ src/popup.js
@@ -1,16 +1,32 @@
 // request all settings from storage when popup is opened
 // and update settings when necessary
-chrome.storage.sync.get('settings', function(response){
-    for(let key in response.settings.youtube){
+chrome.storage.sync.get('settings', function (response) {
+
+    // code for YouTube
+    for (let key in response.settings.youtube) {
         var current = document.getElementById(key)
-        
+
         // check or uncheck checkboxes based on settings
         current.checked = response.settings.youtube[key].hide
 
         // add EventListener that updates the settings
-        current.addEventListener("click", function(){
+        current.addEventListener("click", function () {
             response.settings.youtube[key].hide = !response.settings.youtube[key].hide
-            chrome.storage.sync.set({settings: response.settings})
+            chrome.storage.sync.set({ settings: response.settings })
         })
     }
+})
+
+// code for general extension
+chrome.storage.sync.get('show', function (response) {
+    var current = document.getElementById("extension")
+
+    // check or uncheck checkboxes based on settings
+    current.checked = response.show
+
+    // add EventListener that updates the settings
+    current.addEventListener("click", function () {
+        response.show = !response.show
+        chrome.storage.sync.set({ settings: response.show })
+    })
 })
