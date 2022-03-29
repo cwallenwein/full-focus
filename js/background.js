6f15e42ca4f13cb3b2d82254096a653d35594924
Christian Wallenwein <chris.wallenwein@gmail.com>
2022-03-29 08:00:20 +0000

    refactor background.js

--- js/background.js
+++ js/background.js
@@ -4,72 +4,76 @@ console.log("background running");
 
 // TODO not all elements are about hiding and showing stuff, for example enabling autoplay. Maybe find another name
 
-// when the extension is installed, show everything
-chrome.runtime.onInstalled.addListener(function () {
-  let activeOnStart = false;
-  chrome.storage.sync.set({ active: activeOnStart });
-  updateIcon(activeOnStart);
+chrome.runtime.onInstalled.addListener(setDefaultSettings());
 
-  chrome.storage.sync.set({
-    settings: {
-      homepage: {
-        hide: false,
-      },
-      comments: {
-        hide: false,
-      },
-      playlists: {
-        hide: false,
-      },
-      recommendations: {
-        hide: false,
-      },
-      merch: {
-        hide: false,
-      },
-      recommendationsAfterVideo: {
-        hide: false,
-      },
-      autoplay: {
-        hide: false,
-      },
-      chat: {
-        hide: false,
-      },
-    },
-  });
+// send message to content-script when a new tab is opened
+chrome.tabs.onCreated.addListener(function (tab) {
+  requestUpdateForTab(tab.id, tab.url);
 });
 
-function sendStatus(pTabID, pTabURL) {
+// send message to content-script when the tab is updated
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  const finishedUpdatingPage = changeInfo.status === "complete";
+  if (finishedUpdatingPage) {
+    requestUpdateForTab(tabId, tab.url);
+  }
+});
+
+// send message to content-script when the active tab is changed
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  requestUpdateForTab(activeInfo.tabId, undefined);
+});
+
+function requestUpdateForTab(pTabID, pTabURL) {
   chrome.storage.sync.get("active", function (response) {
     let message = {
       type: response.active ? "enableExtension" : "disableExtension",
-      firstTime: true, //this page was just opended for the first time
+      //firstTime: true, //this page was just opended for the first time
       source: "background.js",
       url: pTabURL,
     };
-    console.log(message);
+    console.log("send message to a certain tab", message);
     updateIcon(response.active);
     chrome.tabs.sendMessage(pTabID, message);
   });
 }
 
-// send message to content-script when a new tab is opened
-chrome.tabs.onCreated.addListener(function (tab) {
-  sendStatus(tab.id, tab.url);
-});
-
-// send message to content-script when the tab is updated
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === "complete") {
-    sendStatus(tabId, tab.url);
-  }
-});
-
-chrome.tabs.onActivated.addListener(function (activeInfo) {
-  sendStatus(activeInfo.tabId, undefined);
-});
-
 function updateIcon(showIcon) {
   chrome.browserAction.setIcon({ path: "../img/" + showIcon + ".png" });
+}
+
+function setDefaultSettings() {
+  let activeOnStart = true;
+  let defaultSettings = {
+    settings: {
+      homepage: {
+        hide: true,
+      },
+      comments: {
+        hide: true,
+      },
+      playlists: {
+        hide: true,
+      },
+      recommendations: {
+        hide: true,
+      },
+      merch: {
+        hide: true,
+      },
+      recommendationsAfterVideo: {
+        hide: true,
+      },
+      autoplay: {
+        hide: true,
+      },
+      chat: {
+        hide: true,
+      },
+    },
+  };
+
+  updateIcon(activeOnStart);
+  chrome.storage.sync.set({ active: activeOnStart });
+  chrome.storage.sync.set(defaultSettings);
 }
