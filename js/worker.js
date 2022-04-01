e0bcf77729411524d5f632c960eeb87ddb12cb29
Christian Wallenwein <chris.wallenwein@gmail.com>
2022-04-01 13:56:15 +0000

    disable extension when not on YT

--- js/worker.js
+++ js/worker.js
@@ -1,13 +1,25 @@
 chrome.runtime.onInstalled.addListener(initializeState);
 
 chrome.tabs.onCreated.addListener(function (tab) {
-  sendCurrentStateToTab(tab.id, tab.url);
+  if (tab.url.startsWith("https://www.youtube.com/")) {
+    chrome.action.enable(tab.id);
+    sendCurrentStateToTab(tab.id);
+  } else {
+    chrome.action.disable(tab.id);
+  }
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   const finishedUpdatingPage = changeInfo.status === "complete";
   if (finishedUpdatingPage) {
-    sendCurrentStateToTab(tabId, tab.url);
+    if (tab.url.startsWith("https://www.youtube.com/")) {
+      chrome.action.enable(tabId);
+      sendCurrentStateToTab(tabId);
+    } else {
+      chrome.action.disable(tabId);
+    }
+  } else {
+    chrome.action.disable(tabId);
   }
 });
 
@@ -18,10 +30,8 @@ function initializeState() {
   chrome.storage.sync.set(state);
 }
 
-function sendCurrentStateToTab(id, url) {
-  if (url.startsWith("https://www.youtube.com/")) {
-    chrome.storage.sync.get(null, function (state) {
-      chrome.tabs.sendMessage(id, state);
-    });
-  }
+function sendCurrentStateToTab(id) {
+  chrome.storage.sync.get(null, function (state) {
+    chrome.tabs.sendMessage(id, state);
+  });
 }
