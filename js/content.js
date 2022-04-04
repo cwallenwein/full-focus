cb2ee9cb3eb150e9a7779e3c1016adcbc27cf752
Christian Wallenwein <chris.wallenwein@gmail.com>
2022-04-04 17:50:25 +0000

    allow hiding search results

--- js/content.js
+++ js/content.js
@@ -1,16 +1,74 @@
 "use strict";
 
-const box = document.createElement("div");
-box.style.cssText = "position:absolute;width:50px;height:50px;background:#555555;z-index:5000";
-document.body.insertBefore(box, document.body.firstChild);
+// TODO: detect URL change
+
+injectCss();
 
 chrome.runtime.onMessage.addListener(handleStateUpdate);
 
-function handleStateUpdate(newState, sender, sendResponse) {
-  if (newState.extension_active) {
-    box.style.background = "#00FF00";
-  } else {
-    box.style.background = "#FF0000";
-  }
+function handleStateUpdate(state, sender, sendResponse) {
+  toggleCss(state);
   sendResponse(true);
+}
+
+function injectCss() {
+  if (showingHomepage() || showingSearchResults()) {
+    injectCssHomepage();
+  } else if (showingVideoPlayer()) {
+  }
+}
+
+function toggleCss(state) {
+  for (const stylesheet of document.styleSheets) {
+    if (Object.keys(state).includes(stylesheet.title)) {
+      stylesheet.disabled = !state[stylesheet.title];
+    }
+  }
+}
+
+function showingHomepage() {
+  return location.href == "https://www.youtube.com/" || location.href.startsWith("https://www.youtube.com/#");
+}
+
+function showingSearchResults() {
+  return location.href.startsWith("https://www.youtube.com/results");
+}
+
+function showingVideoPlayer() {
+  return location.href.startsWith("https://www.youtube.com/watch");
+}
+
+function injectCssHomepage() {
+  let style = document.createElement("style");
+  style.title = "extension_active";
+
+  style.textContent = `html,
+  body {
+    margin: 0; height: 100%;
+    overflow: hidden;
+  }
+  
+  #start,#guide,#chips-wrapper,#items,#masthead-ad {
+    display: none !important;
+  }
+
+  ytd-browse #contents{
+    display: none !important;
+  }
+  
+  #center {
+    position: absolute;
+    left: 15vw;
+    /*top: 45vh; */
+    width: 70vw !important;
+    height: 10vh;
+  }
+
+  
+  #end {
+    position: absolute;
+    top: 8px; right: 0px;
+  }`;
+
+  document.documentElement.prepend(style);
 }
